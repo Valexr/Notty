4b285e11d044718e82bd196f58f1cfc0a186e886
Valexr <valexr@gmail.com>
2023-02-10 20:28:57 +0300

    fix pages

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,11 @@
     import { pathparams } from "$lib/router/path";
     import { navigate } from "$lib/router/navigate";
     import Route from "$lib/router/Route.svelte";
-    import Home from "$pages/home.svelte";
-    import Editor from "$pages/editor.svelte";
-    import Search from "$pages/search.svelte";
-    import Archive from "$pages/archive.svelte";
-    import Settings from "$pages/settings.svelte";
+    import Home from "$pages/Home.svelte";
+    import Editor from "$pages/Editor.svelte";
+    import Search from "$pages/Search.svelte";
+    import Archive from "$pages/Archive.svelte";
+    import Settings from "$pages/Settings.svelte";
     import ContextMenu from "$lib/components/ContextMenu.svelte";
 
     // Share target action and initial redirect
